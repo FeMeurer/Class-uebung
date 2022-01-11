af51a3e7136df60cc0a96f7a48e8ecb860605a52
Felix Meurer <f.meurer@telekom.de>
2022-01-11 15:59:03 +0100

    Test

--- src/main/java/de/telekom/sea7/Main.java
+++ src/main/java/de/telekom/sea7/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Person person_herbert = new Person("Herbert","Müller","m",3,6,1984,11111111111l); // l hinter Zahl weist auf long hin
-		Person person_Jutta = new Person("Jutta","Bauer","w",17,3,1970,12346678912l);
+		Person person_Jutta = new Person("Jutta","Bauer","w",17,3,1970,12345678912l);
 		Person person_herbert2 = new Person("Herbert","Müller","m",3,6,1984,11111111111l); 
 		
 		Punkt punkt1 = new Punkt(30,28);
